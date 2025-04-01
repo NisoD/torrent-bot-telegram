dd982799431b361017ee0fb673c04c13b6bf93c1
nisoD <danja23@gmail.com>
2025-04-01 11:17:44 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Telegram Torrent Downloader (Go)
+
+A Telegram bot written in Go that downloads torrents and uploads them to AWS S3.
+
+## 🚀 Features
+- Accepts magnet links via Telegram.
+- Downloads torrents using `aria2`.
+- Uploads completed files to AWS S3.
+- Sends a download link back to the user.
+
+## 🛠️ Setup
+1. **Clone the repo**  
+   ```sh
+   git clone https://github.com/yourusername/tg-torrent-downloader.git
+   cd tg-torrent-downloader
+
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+	"os"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized as %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello! I'm your bot.")
+			bot.Send(msg)
+		}
+	}
+}
