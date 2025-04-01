188e60733fbe3a445c92893b8d08641a9487f025
Daniel Nisnevich <danja23@gmail.com>
2025-04-01 23:58:00 +0300

    Update server.go

--- server/server.go
+++ server/server.go
@@ -5,19 +5,17 @@ import (
 	"net/http"
 )
 
-// Server represents the HTTP server for the application
+// HTTP server for the app
 type Server struct {
 	router *http.ServeMux
 }
 
-// NewServer creates a new server instance
 func NewServer() *Server {
 	return &Server{
 		router: http.NewServeMux(),
 	}
 }
 
-// Start starts the HTTP server
 func (s *Server) Start(addr string) error {
 	log.Printf("Starting server on %s", addr)
 	return http.ListenAndServe(addr, s.router)
