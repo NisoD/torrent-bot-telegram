12dff976015b212faae7e72d06b82b638d01c731
Daniel Nisnevich <danja23@gmail.com>
2025-04-02 00:00:23 +0300

    Update logger.go

--- server/logger.go
+++ server/logger.go
@@ -8,7 +8,6 @@ import (
 	"time"
 )
 
-// Logger handles logging operations
 type Logger struct {
 	logFile   *os.File
 	logger    *log.Logger
@@ -16,25 +15,20 @@ type Logger struct {
 	debugMode bool
 }
 
-// NewLogger creates a new logger instance
 func NewLogger(logPath string, debugMode bool) (*Logger, error) {
-	// Ensure log directory exists
+
 	if err := os.MkdirAll(logPath, 0755); err != nil {
 		return nil, err
 	}
 
-	// Create log file with timestamp in name
 	timestamp := time.Now().Format("2006-01-02")
 	logFileName := fmt.Sprintf("bot-log-%s.txt", timestamp)
 	logFilePath := filepath.Join(logPath, logFileName)
 
-	// Open log file
 	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		return nil, err
 	}
-
-	// Create logger
 	logger := log.New(logFile, "", log.LstdFlags)
 
 	return &Logger{
@@ -45,25 +39,22 @@ func NewLogger(logPath string, debugMode bool) (*Logger, error) {
 	}, nil
 }
 
-// LogInfo logs an informational message
+// Write
 func (l *Logger) LogInfo(format string, v ...interface{}) {
 	message := fmt.Sprintf(format, v...)
 	l.logger.Printf("[INFO] %s", message)
 
-	// Also print to console
 	log.Printf("[INFO] %s", message)
 }
 
-// LogError logs an error message
 func (l *Logger) LogError(format string, v ...interface{}) {
 	message := fmt.Sprintf(format, v...)
 	l.logger.Printf("[ERROR] %s", message)
 
-	// Also print to console
 	log.Printf("[ERROR] %s", message)
 }
 
-// LogDebug logs a debug message (only if debug mode is enabled)
+//  debug mode
 func (l *Logger) LogDebug(format string, v ...interface{}) {
 	if !l.debugMode {
 		return
@@ -72,11 +63,9 @@ func (l *Logger) LogDebug(format string, v ...interface{}) {
 	message := fmt.Sprintf(format, v...)
 	l.logger.Printf("[DEBUG] %s", message)
 
-	// Also print to console
 	log.Printf("[DEBUG] %s", message)
 }
 
-// Close closes the log file
 func (l *Logger) Close() error {
 	return l.logFile.Close()
 }
